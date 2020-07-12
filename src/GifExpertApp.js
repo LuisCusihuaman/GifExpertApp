a9f558313b291c658899c448c2498efd2763ecfd
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-12 17:35:55 -0300

    test: should have the image equal to the url and alt of the props

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-	const [categories, setCategories] = useState(["One Punch"]);
-	//Esto lo que hace cuando termina setCargories es setearlo a undefined porque setea el estado a lo que devuelve la funcion
-	// const handleAdd = () => {
-	// setCategories((categories) => {
-	// console.log(categories);
-	// categories.push("One Piece");
-	// });
-	// };
-	// console.log(categories);
-	// const handleAdd = () => {
-	// 	setCategories(["One Piece", ...categories]);
-	// };
-	return (
-		<>
-			<h2>GifExpertApp</h2>
-			<AddCategory setCategories={setCategories} />
-			<hr />
-			{/* <button onClick={handleAdd}>Agregar One Piece</button> */}
-			<ol>
-				{categories.map((category) => (
-					<GifGrid key={category} category={category} />
-				))}
-			</ol>
-		</>
-	);
+  const [categories, setCategories] = useState(['One Punch']);
+  //Esto lo que hace cuando termina setCargories es setearlo a undefined porque setea el estado a lo que devuelve la funcion
+  // const handleAdd = () => {
+  // setCategories((categories) => {
+  // console.log(categories);
+  // categories.push("One Piece");
+  // });
+  // };
+  // console.log(categories);
+  // const handleAdd = () => {
+  // 	setCategories(["One Piece", ...categories]);
+  // };
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+      {/* <button onClick={handleAdd}>Agregar One Piece</button> */}
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
 };
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -1,13 +1,22 @@
-import React from "react";
-import { shallow } from "enzyme";
-import "@testing-library/dom";
-import { GifGridItem } from "../../components/GifGridItem";
+import React from 'react';
+import { shallow } from 'enzyme';
+import '@testing-library/dom';
+import { GifGridItem } from '../../components/GifGridItem';
 
-describe("Pruebas en <GifGridItem/>", () => {
-	test("debe mostrar <GifGridItem /> correctamente ", () => {
-		const title = "Un titulo";
-		const url = "https://localhost/algo,jpg";
-		const wrapper = shallow(<GifGridItem title={title} url={url} />);
-		expect(wrapper).toMatchSnapshot();
-	});
+describe('Pruebas en <GifGridItem/>', () => {
+  const title = 'Un titulo';
+  const url = 'https://localhost/algo,jpg';
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
+  test('debe mostrar <GifGridItem /> correctamente ', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  test('debe de tener un parrafo con el title', () => {
+    const p = wrapper.find('p');
+    expect(p.text().trim()).toBe(title);
+  });
+  test('debe tener la imagen igual a la url y alt  de los props', () => {
+    const img = wrapper.find('img');
+    expect(img.prop('src')).toBe(url);
+    expect(img.prop('alt')).toBe(title);
+  });
 });
