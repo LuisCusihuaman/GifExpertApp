b0bf9dbf15334ee3f2d01a9a4f6664bc20ad5636
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-12 01:57:30 -0300

    chore: addCategory component

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
-	// const categories = ["One Punch", "Samurai X", "Dragon Ball"];
 	const [categories, setCategories] = useState([
 		"One Punch",
 		"Samurai X",
 		"Dragon Ball",
 	]);
 	//Esto lo que hace cuando termina setCargories es setearlo a undefined porque setea el estado a lo que devuelve la funcion
-	/* const handleAdd = () => {
-		setCategories((categories) => {
-			console.log(categories);
-			categories.push("One Piece");
-		});
-	};
-    console.log(categories); */
-	const handleAdd = () => {
-		setCategories(["One Piece", ...categories]);
-	};
+	// const handleAdd = () => {
+	// setCategories((categories) => {
+	// console.log(categories);
+	// categories.push("One Piece");
+	// });
+	// };
+	// console.log(categories);
+	// const handleAdd = () => {
+	// 	setCategories(["One Piece", ...categories]);
+	// };
 	return (
 		<>
 			<h2>GifExpertApp</h2>
+			<AddCategory setCategories={setCategories} />
 			<hr />
-			<button onClick={handleAdd}>Agregar One Piece</button>
+			{/* <button onClick={handleAdd}>Agregar One Piece</button> */}
 			<ol>
 				{categories.map((category) => (
 					<li key={category}>{category}</li>
